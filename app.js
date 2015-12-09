5365055fea31bac4d1edb14e1005fdfc865521d3
nikhil.thakur <In2s4me!>
2015-12-09 14:13:42 -0500

    Added AngelloHelper as a resource for MainCtrl and added a css file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var myModule = angular.module('Angello',[]);
-myModule.factory('AngelloHelper', function () {
-	var buildIndex = function (source, property) {
+var myModule = angular.module('Angello', []);
+
+myModule.factory('AngelloHelper', function() {
+    var buildIndex = function (source, property) {
         var tempArray = [];
 
         for (var i = 0, len = source.length; i < len; ++i) {
@@ -16,68 +17,69 @@ myModule.factory('AngelloHelper', function () {
 });
 
 myModule.service('AngelloModel', function() {
-	var service = this;
-    statuses = [
-        {name: 'Back Log'},
-        {name: 'To Do'},
-        {name: 'In Progress'},
-        {name: 'Code Review'},
-        {name: 'QA Review'},
-        {name: 'Verified'},
-        {name: 'Done'}
-    ],
-    types = [
-        {name: 'Feature'},
-        {name: 'Enhancement'},
-        {name: 'Bug'},
-        {name: 'Spike'}
+    var service = this,
+        statuses = [
+            {name: 'Back Log'},
+            {name: 'To Do'},
+            {name: 'In Progress'},
+            {name: 'Code Review'},
+            {name: 'QA Review'},
+            {name: 'Verified'},
+            {name: 'Done'}
         ],
-	stories=[
-		{
-		title : 'First story',
-		description: 'My first story description',
-		criteria: 'Criteria pending',
-		status: 'Back log',
-		type: 'pending',
-		reporter: 'Nik',
-		assignee: 'Nik'
-	},
-	{
-		title : 'Second story',
-		description: 'My second story description',
-		criteria: 'Criteria pending',
-		status: 'Back log',
-		type: 'pending',
-		reporter: 'Nik',
-		assignee: 'Nik'
-	},
-	{
-		title : 'Another story',
-		description: 'Another story description',
-		criteria: 'Criteria pending',
-		status: 'Back log',
-		type: 'pending',
-		reporter: 'Nik',
-		assignee: 'Nik'
-	}];
+        types = [
+            {name: 'Feature'},
+            {name: 'Enhancement'},
+            {name: 'Bug'},
+            {name: 'Spike'}
+        ],
+        stories = [
+            {
+                title: 'First story',
+                description: 'Our first story.',
+                criteria: 'Criteria pending.',
+                status: 'To Do',
+                type: 'Feature',
+                reporter: 'Lukas Ruebbelke',
+                assignee: 'Brian Ford'
+            },
+            {
+                title: 'Second story',
+                description: 'Do something.',
+                criteria: 'Criteria pending.',
+                status: 'Back Log',
+                type: 'Feature',
+                reporter: 'Lukas Ruebbelke',
+                assignee: 'Brian Ford'
+            },
+            {
+                title: 'Another story',
+                description: 'Just one more.',
+                criteria: 'Criteria pending.',
+                status: 'Code Review',
+                type: 'Enhancement',
+                reporter: 'Lukas Ruebbelke',
+                assignee: 'Brian Ford'
+            }
+        ];
 
-	service.getStrories = function(){
-		return stories;
-	};
-	service.getStatuses = function () {
-		return statuses;
-	};
+    service.getStatuses = function () {
+        return statuses;
+    };
 
-	service.getTypes = function () {
-		return types;
-	};
+    service.getTypes = function () {
+        return types;
+    };
 
+    service.getStories = function () {
+        return stories;
+    };
+});
 
-} );
+myModule.controller('MainCtrl', function(AngelloModel, AngelloHelper) {
+    var main = this;
 
-myModule.controller('MainCtrl', function(AngelloModel) {
-	var main = this;
-	main.stories = AngelloModel.getStrories();
+    main.types = AngelloModel.getTypes();
     main.statuses = AngelloModel.getStatuses();
     main.stories = AngelloModel.getStories();
     main.typesIndex = AngelloHelper.buildIndex(main.types, 'name');
@@ -90,16 +92,16 @@ myModule.controller('MainCtrl', function(AngelloModel) {
     };
 
     main.createStory = function() {
-		main.stories.push({
-			title : 'New story',
-			description: 'Description pending',
-			criteria: 'Criteria pending',
-			status: 'Back log',
-			type: 'pending',
-			reporter: 'pending',
-			assignee: 'pending'		
-		})
-	};
+        main.stories.push({
+            title: 'New Story',
+            description: 'Description pending.',
+            criteria: 'Criteria pending.',
+            status: 'Back Log',
+            type: 'Feature',
+            reporter: 'Pending',
+            assignee: 'Pending'
+        });
+    };
 
     main.setCurrentStatus = function (status) {
         if (typeof main.currentStory !== 'undefined') {
@@ -113,10 +115,11 @@ myModule.controller('MainCtrl', function(AngelloModel) {
         }
     };
 });
+
 myModule.directive('story', function() {
-	return {
-	       scope: true,
-	       replace: true,
-	       template: '<div><h4>{{story.title}}</h4><p>{{story.description}}</p></div>'
-	}
+    return {
+        scope: true,
+        replace: true,
+        template: '<div><h4>{{story.title}}</h4><p>{{story.description}}</p></div>'
+    }
 });
